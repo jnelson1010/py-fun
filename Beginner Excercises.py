e6af854eb98556c0d5b9218795f7fa161a953818
Jason Nelson <jasonnelson@live.com>
2019-02-26 11:29:03 -0700

    This is the first revision

---
+++ Beginner Excercises.py
@@ -0,0 +1,74 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# practice python beginner excercises"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "\n",
+      "Jason, You will be 100 in the year 2081. \n",
+      "Jason, You will be 100 in the year 2081. \n",
+      "Jason, You will be 100 in the year 2081. \n",
+      "Jason, You will be 100 in the year 2081. \n"
+     ]
+    }
+   ],
+   "source": [
+    "# Excercise #1\n",
+    "name = input('What is your name? ')\n",
+    "age = int(input('What is your age? '))\n",
+    "answer = (100 - age + 2018)\n",
+    "message = int(input('How many times would you like to see this message? '))\n",
+    "print(('\\n' '' + name + ', You will be 100 in the year ' + str(answer) + '. ')* message)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
